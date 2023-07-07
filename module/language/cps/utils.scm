f8eaee90519207d0090bc9f74f5e677bb4ff8a64
Andy Wingo <wingo@pobox.com>
2023-07-07 11:30:39 +0200

    Fix representation for fadd/immediate
    * module/language/cps/utils.scm (compute-var-representations): Add
    fadd/immediate.

--- module/language/cps/utils.scm
+++ module/language/cps/utils.scm
@@ -397,6 +397,7 @@ by a label, respectively."
              (($ $primcall (or 'scm->f64 'load-f64 's64->f64
                                'f32-ref 'f64-ref
                                'fadd 'fsub 'fmul 'fdiv 'fsqrt 'fabs
+                               'fadd/immediate
                                'ffloor 'fceiling
                                'fsin 'fcos 'ftan 'fasin 'facos 'fatan 'fatan2))
               (intmap-add representations var 'f64))
